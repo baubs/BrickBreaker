4e12f95ff9c53ac51cd62515cb24db798d82e869
Brandon Aubrey <baubrey@nd.edu>
2016-12-03 11:40:59 -0500

    Brick collision detection from bottom only
    First take on ball hitting bottom of bricks, needs more work. Also need
    to do sides and top.

--- ping-pong.js
+++ ping-pong.js
@@ -27,10 +27,10 @@ window.onload = function init()
 	for(c = 0; c < numBricks/5; c ++){
 		for(r = 0; r < 5; r++){
 			var brick = {
-				'x': -.9, 'y':0, 'hits': 1
+				'x': -.9, 'y':.95, 'hits': 1
 			}
 			brick.x = brick.x + (.20)*c;
-			brick.y = 0 + (-.1)*r;
+			brick.y = brick.y + (-.1)*r;
 			brick.hits = 1;
 			bricks[c + (r*10)] = brick;
 		}
@@ -63,10 +63,10 @@ window.onload = function init()
 	vertices.push(vec2(.125, -.95));
     
 	//vertecies for brick
-	vertices.push(vec2(.1, 1));
-	vertices.push(vec2(-.1, 1));
-	vertices.push(vec2(-.1, .9));
-	vertices.push(vec2(.1, .9));
+	vertices.push(vec2(.1, .05));
+	vertices.push(vec2(-.1, .05));
+	vertices.push(vec2(-.1, -.05));
+	vertices.push(vec2(.1, -.05));
 	
     //  Configure WebGL
     gl.viewport( 0, 0, canvas.width, canvas.height );
@@ -157,11 +157,25 @@ window.onload = function init()
     render();
 };
 
+function colide(){
+	var i;
+	for(i = 0; i < numBricks; i++){
+		if( bricks[i].hits >=1 && ballY + yVel >= bricks[i].y - .075 && ballX > bricks[i].x - .075 && ballX < bricks[i].x + .075){
+			console.log("Ball(x,y): ("+ballX+","+ballY+")");
+			console.log("Brick(x,y): ("+bricks[i].x+","+bricks[i].y+")");
+			ballY = bricks[i].y - .075;
+			yVel = yVel*-1;
+			bricks[i].hits --;
+		}
+	}
+}
 
 function render() {
 	if(over == 0){
 		
 		gl.clear( gl.COLOR_BUFFER_BIT );
+		
+		colide();
 		//Game over
 		if(ballY <= -.98){
 			xVel = 0;
@@ -171,24 +185,24 @@ function render() {
 		}
 	
 		//Figure out ball displacement
-		if(ballX + xVel >= .95){
-			ballX = .95;
+		if(ballX + xVel >= .97){
+			ballX = .97;
 			xVel = xVel*(-1);
 		}
-		else if(ballX + xVel <= -.95){
-			ballX = -.95;
+		else if(ballX + xVel <= -.97){
+			ballX = -.97;
 			xVel = xVel*(-1);
 		}
 		else{
 			ballX += xVel;
 		}
-		if(ballY + yVel >= .95){
-			ballY = .95;
+		if(ballY + yVel >= .97){
+			ballY = .97;
 			yVel = yVel*(-1);
 		}
-		else if(ballY + yVel <= -.9){
+		else if(ballY + yVel <= -.92){
 			if(ballX >= padDir -.125 && ballX <= padDir + .125){
-				ballY = -.9;
+				ballY = -.92;
 				yVel = yVel*(-1);
 				saves += 1;
 				label.innerHTML = "Bounces: " + saves;
@@ -202,13 +216,7 @@ function render() {
 		}
 	
 	
-		//Ball
-		gl.uniform4fv( colorLoc, vec4(0.4, 0.4, 1.0, 1.0) );
-		dispX = ballX;
-		dispY = ballY;
-		gl.uniform1f(dispXLoc, dispX);
-		gl.uniform1f(dispYLoc, dispY);
-		gl.drawArrays( gl.TRIANGLE_FAN, 0, numVertices + 1);
+		
 	
 		//Paddle
 		gl.uniform4fv( colorLoc, vec4(1.0, 0.4, 0.4, 1.0) );
@@ -221,12 +229,24 @@ function render() {
 		//Brick
 		var i;
 		for(i = 0; i < numBricks; i ++){
-			//console.log(bricks[i].x - .1);
-			gl.uniform4fv( colorLoc, vec4(Math.random(), Math.random(), Math.random(), 1.0) );
-			gl.uniform1f(dispXLoc, bricks[i].x);
-			gl.uniform1f(dispYLoc, bricks[i].y);
-			gl.drawArrays( gl.TRIANGLE_FAN, numVertices +5, 4);
+			if(bricks[i].hits >= 1){
+				//console.log(bricks[i].x - .1);
+				//gl.uniform4fv( colorLoc, vec4(Math.random(), Math.random(), Math.random(), 1.0) );
+				gl.uniform4fv( colorLoc, vec4(0.0, 0.0, 0.0, 1.0) );
+		
+				gl.uniform1f(dispXLoc, bricks[i].x);
+				gl.uniform1f(dispYLoc, bricks[i].y);
+				gl.drawArrays( gl.TRIANGLE_FAN, numVertices +5, 4);
+			}
 		}
+		
+		//Ball
+		gl.uniform4fv( colorLoc, vec4(0.4, 0.4, 1.0, 1.0) );
+		dispX = ballX;
+		dispY = ballY;
+		gl.uniform1f(dispXLoc, dispX);
+		gl.uniform1f(dispYLoc, dispY);
+		gl.drawArrays( gl.TRIANGLE_FAN, 0, numVertices + 1);
 	
 		setTimeout(
 			function () {requestAnimFrame( render );},
